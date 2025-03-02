9aee343b8409b73704c4f1f6dc20160efc0c71aa
hhyufan <1838248655@qq.com>
2025-03-02 19:24:13 +0800

    fix: 增加首页显示

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const httpProxy = require('http-proxy');
+const Koa = require('koa');
+const app = new Koa();
 
-// 配置参数
-const TARGET_URL = 'http://cmyam.net:65533'; // 目标服务地址
-
-const proxy = httpProxy.createProxyServer({
-    target: TARGET_URL,
-    changeOrigin: true, // 修改请求头中的 Origin 为目标地址
+app.use(async ctx => {
+    ctx.body = 'Hello Vercel';
 });
 
-module.exports = (req, res) => {
-    // 仅处理目标域名的请求
-    proxy.web(req, res, (err) => {
-        if (err) {
-            console.error('代理错误:', err.message);
-            res.status(500).send('代理错误');
-        }
-    });
-};
-
-// 错误处理
-proxy.on('error', (err) => {
-    console.error('代理错误:', err.message);
+app.listen(3008, () => {
+    console.log('3008项目启动')
 });
