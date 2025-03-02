e6332ff0fddfc2403895774d9d82f20238d9b424
hhyufan <1838248655@qq.com>
2025-03-02 19:19:26 +0800

    fix: 使用vercel自身的路由规则进行测试

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,21 +1,25 @@
 const httpProxy = require('http-proxy');
 
-const TARGET_URL = 'http://cmyam.net:65533';
+const TARGET_URL = 'http://cmyam.net:65533'; // 目标服务地址
 const proxy = httpProxy.createProxyServer({
     target: TARGET_URL,
-    changeOrigin: true,
+    changeOrigin: true, // 修改请求头中的 Origin 为目标地址
 });
 
-// 处理所有路径请求
 module.exports = (req, res) => {
+    // 打印请求路径，用于调试
+    console.log('Proxying request to:', req.url);
+
+    // 代理请求
     proxy.web(req, res, (err) => {
         if (err) {
-            console.error('Proxy error:', err);
+            console.error('Proxy error:', err.message);
             res.status(500).send('Proxy error');
         }
     });
 };
 
+// 错误处理
 proxy.on('error', (err) => {
-    console.error('Proxy error:', err);
+    console.error('Proxy error:', err.message);
 });
