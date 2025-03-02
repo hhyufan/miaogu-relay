4e0a921922a661495f92c1e6d44bad7bfaba1d2f
hhyufan <1838248655@qq.com>
2025-03-02 21:08:33 +0800

    revert: 回滚，使用koa-proxy库

---
+++ index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const proxy = require('koa-proxy');
+const app = new Koa();
+
+// 代理配置
+app.use(proxy({
+    host: 'http://cmyam.net:65533', // 后端服务的地址
+    match: /^\/api\//, // 匹配以 /api/ 开头的请求路径
+}));
+
+// 其他路由处理
+app.use(async ctx => {
+    ctx.body = 'Hello Vercel';
+});
+
+app.listen(3008, () => {
+    console.log('3008项目启动');
+});
