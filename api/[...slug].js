28718dc5b1f55b1e73dd0eff0b87866413b7da1c
hhyufan <1838248655@qq.com>
2025-03-02 20:01:34 +0800

    feat: 增加CORS配置

--- api/[...slug].js
+++ api/[...slug].js
@@ -6,9 +6,22 @@ const proxy = httpProxy.createProxyServer({
     changeOrigin: true, // 修改请求头中的 Origin 为目标地址
 });
 
+// CORS 处理函数
+const setCORSHeaders = (res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // 允许所有源，或替换为特定源
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // 允许的 HTTP 方法
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // 允许的自定义头部
+};
+
 module.exports = (req, res) => {
     // 打印请求路径，用于调试
     console.log('Proxying request to:', req.url);
+
+    // 处理预检请求
+    if (req.method === 'OPTIONS') {
+        setCORSHeaders(res);
+        return res.status(204).end(); // 结束预检请求
+    }
 
     // 代理请求
     proxy.web(req, res, (err) => {
@@ -16,6 +29,11 @@ module.exports = (req, res) => {
             console.error('Proxy error:', err.message);
             res.status(500).send('Proxy error');
         }
+    });
+
+    // 设置 CORS 头部
+    proxy.on('proxyRes', (proxyRes, req, res) => {
+        setCORSHeaders(res); // 在代理响应中设置 CORS 头部
     });
 };
 
